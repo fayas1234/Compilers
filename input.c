42d6317e3f4430edbeb5ae9d1e4cb15905113de5
FAYAS AHAMED F <94542226+fayas1234@users.noreply.github.com>
2023-09-03 15:31:20 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,20 @@
+//Test program
+
+/* This is 
+	a program 
+		to test lexical analyser */
+		
+
+#include <stdio.h>
+
+void main ( )
+{
+	int a , b , c ;
+	
+	a = 10 ;
+	b = 20 ;
+	
+	c = a / b ;
+	
+	printf ( "%d\n" , c ) ;
+}
